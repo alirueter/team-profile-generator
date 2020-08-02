57c3f6936c0b41659f84d62efc8809cfa558261e
alirueter <rueter.alison@gmail.com>
2020-08-02 08:17:21 -0500

    finished index.js

--- Index.js
+++ Index.js
@@ -15,42 +15,82 @@ class generator {
 
     getEmployeeInfo() {
         inquirer
-            .prompt([
-                {
-                    type: "input",
-                    message: "Please enter employee's ID: ",
-                    name: "id"
-                },
-                {
-                    type: "input",
-                    message: "Please enter employee's name: ",
-                    name: "name"
-                },
-                {
-                    type: "input",
-                    message: "Please enter employee's title: ",
-                    name: "title"
-                }
-            ])
-    }
+        .prompt([
+            {
+                type: "input",
+                message: "Please enter employee's ID: ",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "Please enter employee's name: ",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "Please enter employee's title: ",
+                name: "title"
+            }
+        ])
+    };
     
     getManagerInfo(managerInfo) {
         inquirer
-            .prompt([
-                {
-                    type: "input",
-                    message: "Please enter manager's office number: ",
-                    name: "officeNumber"
-                }
-            ])
-            .then(input => {
-                managerInfo.officeNumber = input.officeNumber;
-                return managerInfo;
-            })
-            .then(managerInfo => {
-                console.log(managerInfo);
-                return managerInfo;
-            })
-    }
-    
+        .prompt([
+            {
+                type: "input",
+                message: "Please enter manager's office number: ",
+                name: "officeNumber"
+            }
+        ])
+        .then(input => {
+            managerInfo.officeNumber = input.officeNumber;
+            return managerInfo;
+        })
+        .then(managerInfo => {
+            console.log(managerInfo);
+            return managerInfo;
+        })
+    };
+
+    getEngineerInfo(engineerInfo) {
+        inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Please enter engineer's GitHub username: ",
+                name: "github"
+            }
+        ])
+        .then(input => {
+            engineerInfo.github = input.github;
+            return engineerInfo;
+        })
+        .then(engineerInfo => {
+            console.log(engineerInfo);
+            return engineerInfo;
+        })
+    };
+
+    getInternInfo(internInfo) {
+        inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Please enter intern's school: ",
+                name: "school"
+            }
+        ])
+        .then (input => {
+            internInfo.school = input.school;
+            return internInfo;
+        })
+        .then (internInfo => {
+            console.log(internInfo);
+            return internInfo;
+        })
+    };
 }
+
+const generator = new generator();
+generator.getEmployeeInfo();
